f8eb599917c69d554fbd3f1ebe8fb76b4d4ff27c
SD2435324 <sdaviau@student.rcc.edu>
2016-06-23 02:38:40 -0700

    trtr

---
+++ Hmwk/Assignments1/HelloWorldv2/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Shane L. Daviau
+ * Created on June 23, 2016, 2:32 AM
+ * Purpose: Just some fun
+ */
+
+//System Libraries
+#include <iostream> //Input/Output Library
+using namespace std;//Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the Processed Data
+    cout<<"H   H  EEEEE  LL     LL     OOO"<<endl;
+    cout<<"H   H  EE     LL     LL    O   O"<<endl;
+    cout<<"HHHHH  EEEEE  LL     LL    O   O"<<endl;
+    cout<<"H   H  EE     LL     LL    O   O"<<endl;
+    cout<<"H   H  EEEEE  LLLLL  LLLLL  OOO"<<endl;
+    cout<<"                               "<<endl;
+    cout<<"W   W   OOO   RRR   LL     DDD"<<endl;
+    cout<<"W   W  O   O  R  R  LL     D  D"<<endl;
+    cout<<"W W W  O   O  R  R  LL     D  D"<<endl;
+    cout<<"WW WW  O   O  R R   LL     D  D"<<endl;
+    cout<<"W   W   OOO   R  R  LLLLL  DDD"<<endl;
+    //Exit Stage Right!
+    return 0;
+}
